5fe7acc8ce353f000a28f02232ebce19af631691
Htun Htun Naing <htunnaing@hhn.local>
2023-01-10 13:21:18 +0630

    fix composer download

--- laravel_deploy.sh
+++ laravel_deploy.sh
@@ -24,7 +24,28 @@ sudo yum install -y php-gd php-mbstring php-xml php-dom php-intl php-simplexml
 
 #Install new project from composer (testing only)
 
-cd /var/www/html/
+
+#composer install
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+
+rm composer-setup.php
+
+sudo mv composer.phar /usr/local/bin/composer
+
+cd /var/www/html 
 
 composer create-project laravel/laravel:^8.0 larapp
 cd larapp
